51feb3bd0d3e8eedffe8cc91070f055ae4d66679
hiquinho <henriquefleckh04@gmail.com>
2022-02-02 13:47:15 -0300

    Title models

--- blog_stuff/models.py
+++ blog_stuff/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# For a new model use 'class ...'
+
+class Title(models.Model):
+    """Model responsável por gerenciar os tópicos"""
+    title = models.CharField(max_length = 50)
+    date_added = models.DateTimeField(auto_now_add = True)
+
+
+    def __str__(self):
+        """Devolve a informção"""
+        return self.title
