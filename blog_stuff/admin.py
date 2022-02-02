86b5bcd65f99badd4b23cc1d55a6403dffa8e279
hiquinho <henriquefleckh04@gmail.com>
2022-02-02 13:56:35 -0300

    Text model

--- blog_stuff/admin.py
+++ blog_stuff/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from blog_stuff.models import Title
+from blog_stuff.models import Title, Text
 # Register your models here.
 
 admin.site.register(Title)
+admin.site.register(Text)
--- blog_stuff/models.py
+++ blog_stuff/models.py
@@ -12,3 +12,17 @@ class Title(models.Model):
     def __str__(self):
         """Devolve a informção"""
         return self.title
+
+class Text(models.Model):
+    """Model responsável por gerenciar as informações adicionadas
+        em cada tópico"""
+    title = models.ForeignKey(Title, on_delete = models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        """Devolve a informação"""
+        if(len(self.text)<50):
+            return self.text
+        else:
+            return self.text[:50] + "..."
