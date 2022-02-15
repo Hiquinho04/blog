cce819b10a1d853fb0410f6ba2c1fdd8c36a763d
hiquinho <henriquefleckh04@gmail.com>
2022-02-15 13:14:25 -0300

    Logout

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,8 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'),
                 name='login'),
+    #views.logout é uma função NÃO USAR
+    path('logout/', views.logout_views, name='logout'),
+
     
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth import logout, login
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
+def logout_views(request):
+    """Realiza o logout da conta"""
+    logout(request)
+    return HttpResponseRedirect(reverse('blog_stuff:index'))
